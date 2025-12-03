1e059f196ca743b574607cdcba3c620b5d724f97
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-12-03 09:48:31 +0530

    changed home screen

--- mobile/music-id/App.js
+++ mobile/music-id/App.js
@@ -1,19 +1,49 @@
 import React, { useEffect } from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import api from "./src/services/api";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import ListeningScreen from "./src/screens/ListeningScreen";
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ marginTop: 60, padding: 20 }}>
+      <Text style={{ fontSize: 20, marginBottom: 20 }}>
+        Music Identification App 🎵
+      </Text>
+
+      <Button
+        title="Start Listening"
+        onPress={() => navigation.navigate("Listening")}
+      />
+    </View>
+  );
+}
 
 export default function App() {
   useEffect(() => {
-    api.get("/").then(res => {
-      console.log("Backend says:", res.data);
-    }).catch(err => {
-      console.log("Error:", err.message);
-    });
+    api
+      .get("/")
+      .then((res) => {
+        console.log("Backend says:", res.data);
+      })
+      .catch((err) => {
+        console.log("Error:", err.message);
+      });
   }, []);
 
   return (
-    <View style={{ marginTop: 60 }}>
-      <Text>Music Identification App 🎵</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Listening" component={ListeningScreen} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
