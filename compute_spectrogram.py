5188f31bcaf8a4ffd397c0dc04d9eb87f30f0b83
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-12-23 10:09:27 +0530

    computing stft

--- compute_spectrogram.py
+++ compute_spectrogram.py
@@ -8,3 +8,8 @@ HOP_LENGTH = 2048
 
 data = np.load('preprocessed_waveform.npy')
 
+frequencies, times, Zxx = stft(data, fs=SAMPLING_RATE, nperseg=N_PER_SEG, noverlap=N_PER_SEG - HOP_LENGTH)
+spectrogram = np.abs(Zxx)
+
+print(f"Spectrogram shape: {spectrogram.shape} (freq bins × time frames)")
+
