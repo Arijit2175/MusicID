7fdc4bb0e8d2aa416ccdb0ae12e0946749c8e033
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-12-23 09:55:44 +0530

    recording audio

--- record_audio.py
+++ record_audio.py
@@ -5,3 +5,8 @@ SAMPLING_RATE = 44100
 DURATION = 5           
 CHANNELS = 1   
 
+print(f"Recording {DURATION} seconds of audio at {SAMPLING_RATE} Hz...")
+
+recording = sd.rec(int(DURATION * SAMPLING_RATE), samplerate=SAMPLING_RATE, channels=CHANNELS, dtype='float32')
+sd.wait() 
+
