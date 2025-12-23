04a69a05f7732fcc85ea184a12fcf1179294c9a7
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-12-23 10:53:09 +0530

    fingerprint generation

--- live_identify.py
+++ live_identify.py
@@ -41,3 +41,19 @@ detected_peaks = local_max ^ eroded_background
 peaks = np.argwhere(mask & detected_peaks)
 peaks = peaks[np.argsort(peaks[:, 1])]
 
+fingerprints = []
+for i, anchor in enumerate(peaks):
+    anchor_freq, anchor_time = anchor[0], anchor[1]
+    for j in range(1, FAN_VALUE + 1):
+        if i + j >= len(peaks):
+            break
+        target = peaks[i + j]
+        target_freq, target_time = target[0], target[1]
+        time_delta = target_time - anchor_time
+        if 0 < time_delta <= TARGET_ZONE_TIME:
+            fp_tuple = (int(anchor_freq), int(target_freq), int(time_delta))
+            h = hashlib.sha1(str(fp_tuple).encode('utf-8')).hexdigest()[:20]
+            fingerprints.append((h, int(anchor_time)))
+
+print(f"Generated {len(fingerprints)} live fingerprints.")
+
