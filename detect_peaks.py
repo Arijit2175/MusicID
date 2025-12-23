961504b71ba8b210d6c9239df1a06752b6d2c0dd
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-12-23 10:18:14 +0530

    local max filter

--- detect_peaks.py
+++ detect_peaks.py
@@ -9,3 +9,9 @@ spectrogram = np.load('spectrogram.npy')
 threshold = np.percentile(spectrogram, THRESHOLD_PERCENTILE)
 mask = spectrogram >= threshold
 
+structure = generate_binary_structure(2, 2)
+local_max = maximum_filter(spectrogram, footprint=np.ones((NEIGHBORHOOD_SIZE, NEIGHBORHOOD_SIZE))) == spectrogram
+background = (spectrogram == 0)
+eroded_background = binary_erosion(background, structure=structure, border_value=1)
+detected_peaks = local_max ^ eroded_background
+
